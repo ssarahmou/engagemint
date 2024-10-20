dff3a11539979e2766edbc6a96a8ec1692feb542
Sarah <sarahmou@berkeley.edu>
2024-10-19 17:04:27 -0700

    update2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -45,21 +45,21 @@ export default function Home() {
     // Profile
     <div className="flex flex-row h-screen font-inter-tight">
       {/* Navbar */}
-      <div className="w-[400px] h-full flex flex-col border-r-[2px] border-slate bg-gradient-to-t from-[#CCD7FA] to-[#F9F0F8] bg-white p-12">
+      <div className="w-[400px] h-full flex flex-col border-r-[2px] border-slate bg-gradient-to-t from-[#CCD7FA] to-[#F9F0F8] bg-white p-12 fixed ">
         <div className="font-semibold text-2xl mb-12">Engagemint</div>
         <div className="font-bold text-gray-500 tracking-widest">GENERAL</div>
-        <div className="font-semibold text-black mt-5 text-2xl bg-[#FAF7FC] p-6 rounded-xl border-[2px] border-slate border-opacity-95">Dashboard</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-slate border-opacity-100">Collection</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-slate border-opacity-100">Proposals</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-slate border-opacity-100 mb-20">Notifications</div>
+        <div className="font-semibold text-black mt-5 text-2xl bg-[#FAF7FC] p-6 rounded-xl stroke-[2px] border-slate border-opacity-95">Dashboard</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-slate border-opacity-100">Collection</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-slate border-opacity-100">Proposals</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-slate border-opacity-100 mb-20">Notifications</div>
         <div className="font-bold text-gray-500 tracking-widest">PERSONAL</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-opacity-100">Dashboard</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-slate border-opacity-100">Profile</div>
-        <div className="font-semibold text-gray-500 mt-5 text-2xl p-5 rounded-xl border-slate border-opacity-100">Settings</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-opacity-100">Dashboard</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-slate border-opacity-100">Profile</div>
+        <div className="font-semibold text-gray-500 mt-5 text-2xl hover:bg-[#FAF7FC] hover:stroke-[2px] p-6 rounded-xl border-slate border-opacity-100">Settings</div>
       </div>
-
+    
       {/* Main Content */}
-      <div className="flex flex-col flex-grow">
+      <div className="flex flex-col flex-grow ml-[400px]">
         <div className="font-semibold p-16 text-2xl w-full flex flex-row items-center">    
           <div>Welcome, Kelly!</div>
           <div className="ml-auto bg-[#6585C0] text-white pt-2 pl-8 pr-8 pb-2 rounded-full">15.06 ETH</div>
@@ -75,12 +75,12 @@ export default function Home() {
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+        <DropdownMenuLabel>Media Platform</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-          <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Spotify">Spotify</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="ESPN">ESPN</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Netflix">Netflix</DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
@@ -90,7 +90,144 @@ export default function Home() {
         <div className="pt-8 ml-16 w-11/12">
           <SearchInput />
         </div>
+        <div className="pt-16 pl-20 font-bold text-gray-400 tracking-widest" >ESPN</div>
+        <div className="pl-16">
+            <img src="https://i.imgur.com/y0RLnx9.png"></img>
+        </div>
+        
+        {/* ESPN */}
+        <div className="flex pl-16 pt-6 space-x-8 mr-auto">
+      {/* Card 1 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 ">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">NBA Frenzy</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 GAMES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
       </div>
-    </div>
-  );
+
+      {/* Card 2 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 ">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">MLB Marathon</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 GAMES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+
+      {/* Card 3 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">NFL Frenzy</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 GAMES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+      
+
+                  {/* Card 3 */}
+                  <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Golf Grand Slam</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 ROUNDS WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+
+                  {/* Card 3 */}
+                  <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+      </div>
+      <div className="pt-16 pl-20 font-bold text-gray-400 tracking-widest" >NETFLIX</div>
+        <div className="pl-16 mb-[-10px]">
+            <img src="https://i.imgur.com/sw72kwM.png"></img>
+        </div>
+        
+        {/* ESPN */}
+        <div className="flex pl-16 pt-6 space-x-8 mr-auto">
+      {/* Card 1 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 mb-16">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+
+      {/* Card 2 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 mb-16">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+
+      {/* Card 3 */}
+      <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 mb-16">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+      
+
+                  {/* Card 3 */}
+                  <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 mb-16">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+
+                  {/* Card 3 */}
+                  <div className="bg-[#FAF7FC] rounded-lg shadow-md p-4 mb-16">
+        <div className="text-right text-[#6A8CCA] font-bold text-xl">+100 pts</div>
+        <h2 className="mt-2 text-lg font-semibold">Soccer Showdown</h2>
+        <p className="text-sm font-bold tracking-wider text-gray-500">100 MATCHES WATCHED ON ESPN</p>
+        <div className="mt-4 bg-gray-200 h-32 rounded-md"></div>
+        <button className="mt-4 w-full bg-[#6A8CCA] text-white py-2 rounded-md hover:bg-blue-700">
+          Redeem Points
+        </button>
+      </div>
+      
+      </div>
+        <div className="pl-16 font-semibold text-xl underline">
+        Load More
+        </div>
+        <div className="pt-16 pl-20 font-bold text-gray-400 tracking-widest" > </div>
+        <div className="pl-16 mb-12 justify-center item-center">
+            
+        </div>
+
+      </div>
+      
+      </div>
+
+)
 }
